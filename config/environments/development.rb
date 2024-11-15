e61cfd80b8ff6c8d825ee4336147efc8ce76e658
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-11-15 08:31:28 -0500

    Add config for mailing services

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["MAILER_USERNAME"],
+    password: ENV["MAILER_PASSWORD"],
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    authentication: "login",
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none"
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
